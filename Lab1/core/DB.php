691c5cab51586c2cc6dda53d08c659ab3633e115
ljpuetz1 <ljpuetz@iu.edu>
2024-09-25 12:53:11 -0400

    1.23
    added the DB and Router core pages

---
+++ Lab1/core/DB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Author: Logan Puetz
+ * Date: 9/25/2024
+ * File: DB.php
+ * Description:
+ */
+class DB
+{
+    function connect($db)
+    {
+        $conn = new mysqli($db['host'], $db['username'], $db['password'],
+            $db['database'], $db['port']);
+        if ($conn->connect_error) {
+            die('Connect Error (' . $conn->connect_errno . ') '
+                . $conn->connect_error);
+        }
+        return $conn;
+    }
+}
---
+++ Lab1/core/Router.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Author: Logan Puetz
+ * Date: 9/25/2024
+ * File: Router.php
+ * Description:
+ */
+class Router
+{
+    private $routes = [];
+    function setRoutes($routes)
+    {
+        $this->routes = $routes;
+    }
+    function getFilename($url)
+    {
+        foreach ($this->routes as $route => $file) {
+            if (strpos($url, $route) !== false) {
+                return $file;
+            }
+        }
+    }
+}
