bf8e436babddeb04c8fcd774a8d7a836061ae1ca
ljpuetz1 <ljpuetz@iu.edu>
2024-09-25 12:23:10 -0400

    lab1.1

---
+++ Lab1/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Author: Logan Puetz
+ * Date: 9/25/2024
+ * File: config.php
+ * Description: Config file to connect to the blog database
+ */
+
+$db = [
+    'host' => 'localhost',
+    'database' => 'blog',
+    'username' => 'phpuser',
+    'password' => 'phpuser',
+    'port' => 3306
+];
---
+++ Lab1/routes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Author: Logan Puetz
+ * Date: 9/25/2024
+ * File: routes.php
+ * Description:
+ */
+$routes = [
+    'users' => 'users.php',
+    'comments' => 'comments.php'
+];
